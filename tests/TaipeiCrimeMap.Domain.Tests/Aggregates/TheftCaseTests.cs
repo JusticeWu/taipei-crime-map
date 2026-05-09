ea1623550eb3cf89dc56886cb68ca97f8b0cf4f3
Justice Wu <chengyi.ks@gmail.com>
2026-05-09 12:33:06 +0800

    test: add TheftCase IsDataComplete & Create validation tests

--- tests/TaipeiCrimeMap.Domain.Tests/Aggregates/TheftCaseTests.cs
+++ tests/TaipeiCrimeMap.Domain.Tests/Aggregates/TheftCaseTests.cs
@@ -5,15 +5,19 @@ namespace TaipeiCrimeMap.Domain.Tests.Aggregates;
 
 public class TheftCaseTests
 {
+    private const string ValidLocation = "台北市內湖區成功路五段31號";
+
+
     [Fact]
     public void IsDataComplete_WithValidInputs_ReturnsTrue()
     {
         var theftCase = TheftCase.Create(
             caseNumber: "A123",
             caseType: CaseType.Residential,
-            district: District.ParseFrom("台北市內湖區成功路五段31號")!,
+            district: District.ParseFrom(ValidLocation)!,
             occurredDate: TaiwanDate.Parse("1130101"),
             timeSlot: TimeSlot.Parse("08~18"),
+            rawLocation: ValidLocation,
             coordinate: GeoCoordinate.Create(25.04776, 121.51737));
 
         Assert.True(theftCase.IsDataComplete);
@@ -28,10 +32,82 @@ public class TheftCaseTests
             district: null,
             occurredDate: TaiwanDate.Parse("1130101"),
             timeSlot: TimeSlot.Parse("08~18"),
+            rawLocation: ValidLocation,
             coordinate: GeoCoordinate.Create(25.04776, 121.51737));
 
         Assert.False(theftCase.IsDataComplete);
     }
 
-    
+    [Fact]
+    public void IsDataComplete_WithInvalidOccurredDate_ReturnsFalse()
+    {
+        var theftCase = TheftCase.Create(
+            caseNumber: "A123",
+            caseType: CaseType.Residential,
+            district: District.ParseFrom(ValidLocation)!,
+            occurredDate: TaiwanDate.Parse("invalid"),
+            timeSlot: TimeSlot.Parse("08~18"),
+            rawLocation: ValidLocation,
+            coordinate: GeoCoordinate.Create(25.04776, 121.51737));
+
+        Assert.False(theftCase.IsDataComplete);
+    }
+
+    [Fact]
+    public void IsDataComplete_WithInvalidTimeSlot_ReturnsFalse()
+    {
+        var theftCase = TheftCase.Create(
+            caseNumber: "A123",
+            caseType: CaseType.Residential,
+            district: District.ParseFrom(ValidLocation)!,
+            occurredDate: TaiwanDate.Parse("1130101"),
+            timeSlot: TimeSlot.Parse("18~08"),  // 倒置
+            rawLocation: ValidLocation,
+            coordinate: GeoCoordinate.Create(25.04776, 121.51737));
+
+        Assert.False(theftCase.IsDataComplete);
+    }
+
+    [Fact]
+    public void IsDataComplete_WithNullCaseType_ReturnsFalse()
+    {
+        var theftCase = TheftCase.Create(
+            caseNumber: "A123",
+            caseType: null,
+            district: District.ParseFrom(ValidLocation)!,
+            occurredDate: TaiwanDate.Parse("1130101"),
+            timeSlot: TimeSlot.Parse("08~18"),
+            rawLocation: ValidLocation,
+            coordinate: GeoCoordinate.Create(25.04776, 121.51737));
+
+        Assert.False(theftCase.IsDataComplete);
+    }
+
+    [Fact]
+    public void Create_WithNullOccurredDate_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+            TheftCase.Create(
+                caseNumber: "A123",
+                caseType: CaseType.Residential,
+                district: District.ParseFrom(ValidLocation)!,
+                occurredDate: null!,
+                timeSlot: TimeSlot.Parse("08~18"),
+                rawLocation: ValidLocation,
+                coordinate: GeoCoordinate.Create(25.04776, 121.51737)));
+    }
+
+    [Fact]
+    public void Create_WithNullRawLocation_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            TheftCase.Create(
+                caseNumber: "A123",
+                caseType: CaseType.Residential,
+                district: District.ParseFrom(ValidLocation),
+                occurredDate: TaiwanDate.Parse("1130101"),
+                timeSlot: TimeSlot.Parse("08~18"),
+                rawLocation: null!,
+                coordinate: GeoCoordinate.Create(25.04776, 121.51737)));
+    }
 }
