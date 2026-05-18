e5dc29ed4f3a90c799b6f5a0d3706fc6d7aab632
Justice Wu <chengyi.ks@gmail.com>
2026-05-18 13:11:57 +0800

    test: add AddRangeAsync integration test for InMemoryCrimeRepository

--- tests/TaipeiCrimeMap.Integration.Tests/Repositories/CrimeRepositoryTests.cs
+++ tests/TaipeiCrimeMap.Integration.Tests/Repositories/CrimeRepositoryTests.cs
@@ -37,4 +37,32 @@ public class CrimeRepositoryTests : IClassFixture<CustomWebApplicationFactory>
         result.Should().NotBeNull();
         result!.CaseNumber.Should().Be(theftCase.CaseNumber);
     }
+
+    [Fact]
+    public async Task AddRangeAsync_ShouldPersistAllData()
+    {
+        // Arrange
+        var cases = new List<TheftCase>();
+        for (int i = 0; i < 3; i++)
+        {
+            cases.Add(TheftCase.Create(
+                caseNumber: Guid.NewGuid().ToString(),
+                caseType: CaseType.Residential,
+                district: District.ParseFrom("內湖區"),
+                occurredDate: TaiwanDate.Parse("1130101"),
+                timeSlot: null,
+                rawLocation: "臺北市內湖區成功路五段31號"));
+        }
+
+        // Act
+        await _repository.AddRangeAsync(cases);
+
+        // Assert
+        foreach (var c in cases)
+        {
+            var result = await _repository.GetByIdAsync(c.Id);
+            result.Should().NotBeNull();
+            result!.CaseNumber.Should().Be(c.CaseNumber);
+        }
+    }
 }
