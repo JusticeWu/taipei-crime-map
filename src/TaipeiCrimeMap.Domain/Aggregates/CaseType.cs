6e8502097a3bee99227ccd219552a194aeb3ed54
Justice Wu <chengyi.ks@gmail.com>
2026-05-19 13:45:32 +0800

    feat: add Snatching, Robbery types to CaseType enum

--- src/TaipeiCrimeMap.Domain/Aggregates/CaseType.cs
+++ src/TaipeiCrimeMap.Domain/Aggregates/CaseType.cs
@@ -2,8 +2,10 @@ namespace TaipeiCrimeMap.Domain.Aggregates;
 
 public enum CaseType
 {
-    Residential = 1,
-    Car = 2,
-    Motorcycle = 3,
-    Bicycle = 4
+    Residential = 1,    // 住宅竊盜
+    Car = 2,            // 汽車竊盜
+    Motorcycle = 3,     // 機車竊盜
+    Bicycle = 4,        // 自行車竊盜
+    Snatching = 5,      // 搶奪
+    Robbery = 6         // 強盜
 }
